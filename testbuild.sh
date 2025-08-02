5c29001f123b3ce3833f99e4506e3abde6f3a12c
loudnl <LouDnl@users.noreply.github.com>
2025-08-02 12:41:32 +0200

    Update testbuild files

--- testbuild.sh
+++ testbuild.sh
@@ -8,7 +8,7 @@ rm build/*.o
 rm build/*.a
 rm build/*.so
 
-g++ USBSID.cpp -c -fPIC $(pkg-config --cflags --libs libusb-1.0) -o build/USBSID.o
-g++ USBSIDInterface.cpp -c -fPIC $(pkg-config --cflags --libs libusb-1.0) -o build/USBSIDInterface.o
+g++ --verbose USBSID.cpp -c -fPIC $(pkg-config --cflags --libs libusb-1.0) -o build/USBSID.o
+g++ --verbose USBSIDInterface.cpp -c -fPIC $(pkg-config --cflags --libs libusb-1.0) -o build/USBSIDInterface.o
 ar rcs build/USBSIDPico.a build/*.o
-g++ -shared -o build/USBSIDPico.so build/*.o $(pkg-config --libs libusb-1.0)
+g++ --verbose -shared -o build/USBSIDPico.so build/*.o $(pkg-config --libs libusb-1.0)
