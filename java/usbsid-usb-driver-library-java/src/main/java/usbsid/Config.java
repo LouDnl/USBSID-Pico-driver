ba30a2efe3dc3e2d62c5331bf1254ad6aef6d739
loudnl <LouDnl@users.noreply.github.com>
2026-09-25 20:28:37 +0200

    Add GPLv3 header to Java driver

--- java/usbsid-usb-driver-library-java/src/main/java/usbsid/Config.java
+++ java/usbsid-usb-driver-library-java/src/main/java/usbsid/Config.java
@@ -1,3 +1,30 @@
+/*
+ * USBSID-Pico is a RPi Pico (RP2040/RP2350) based board for interfacing one
+ * or two MOS SID chips and/or hardware SID emulators over (WEB)USB with your
+ * computer, phone or ASID supporting player.
+ *
+ * Config.java (Java driver)
+ * This file is part of USBSID-Pico-driver (https://github.com/LouDnl/USBSID-Pico-driver)
+ * File author: LouD
+ *
+ * USBSID-Pico-driver
+ * Copyright (C) 2025-2026  LouD
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package usbsid;
 
 import java.util.Arrays;
